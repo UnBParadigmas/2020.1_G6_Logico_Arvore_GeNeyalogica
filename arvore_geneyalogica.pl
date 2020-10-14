43583ea65cbee70192d24b91a234770090460ad3
Heron Rodrigues <heronrs1@gmail.com>
2020-10-13 21:25:29 -0300

    Adicionando funçoes para irmaos e tios

--- arvore_geneyalogica.pl
+++ arvore_geneyalogica.pl
@@ -41,3 +41,11 @@ avohomem(X,Y) :- progenitor(X,Z), progenitor(Z,Y), homem(X).
 netos(X,Y) :- avos(Y,X).
 neto(X,Y) :- netos(X,Y), homem(X).
 neta(X,Y) :- netos(X,Y), mulher(X).
+
+irmaos(X,Y) :- progenitor(Z,X), progenitor(Z,Y).
+irma(X,Y) :- irmaos(X,Y), mulher(X).
+irmao(X,Y) :- irmaos(X,Y), homem(X). % "Para todo X e Y, se Z é progenitor de X e Z é progenitor de Y e X é homem, X é um irmão"
+
+tios(X,Y) :- irmaos(X,Z), progenitor(Z,Y), \+progenitor(X,Y).
+tia(X,Y) :- tios(X,Y), mulher(X). % "\+ = operador NOT"
+tio(X,Y) :- tios(X,Y), homem(X).
