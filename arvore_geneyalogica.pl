27bbf6a45948222c458a5a196e5e97093061e537
Paulo Victor de Menezes Lopes <pv.paulovictor.vp@gmail.com>
2020-10-13 21:17:25 -0300

    Adição das definições e regras básicas.

---
+++ arvore_geneyalogica.pl
@@ -0,0 +1,35 @@
+mulher(pam).
+mulher(liz).
+mulher(pat).
+mulher(ann).
+
+homem(tom).
+homem(bob).
+homem(jim).
+homem(paul).
+homem(john).
+
+progenitor(pam, bob).	% "pais do bob"
+progenitor(tom, bob).
+
+progenitor(pam, paul).	% "pais do paul"
+progenitor(tom, paul).
+
+progenitor(tom, liz).	% "tom é um dos progenitores de liz"
+
+progenitor(bob, ann).
+
+progenitor(bob, pat).
+
+progenitor(pat, jim).
+
+progenitor(liz, john).
+
+antepassado(X,Y) :- progenitor(X,Y); progenitor(X,Z), antepassado(Z,Y).
+
+mae(X,Y) :- progenitor(X,Y), mulher(X).
+pai(X,Y) :- progenitor(X,Y), homem(X).
+
+filhos(X,Y) :- progenitor(Y,X). % "Para todo X e Y, se X é progenitor de Y, então Y é filho de X"
+filho(X,Y) :- filhos(X,Y), homem(X).
+filha(X,Y) :- filhos(X,Y), mulher(X).
